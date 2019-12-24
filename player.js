c94ad5127b6238c2a2470a9b99bbb1e68c807676
Aschaley Ascheneke <45295709+aschalegn@users.noreply.github.com>
2019-12-24 07:13:23 +0200

    redesign

--- player.js
+++ player.js
@@ -1,87 +1,63 @@
-window.onload = ()=>{
-    //MUsic information constructor function.
-    let files = function(musicSource, Image, title, track){
-        this.musicSource = musicSource;
-        this.Image = Image; 
-        this.title = title;
-        this.track = track;
-    }
-
-    let musicSrc = document.querySelectorAll('audio source');
-    let musicS = [];
-    let musicTitle = [];
-    let imageSrc = [];
-    let musicTrack = [];
-
-    //Take out all the data from the audio source file and put it to the rellevant array
-    for (let i = 0; i < musicSrc.length; i++) {
-        musicS.push(musicSrc[i].dataset.src);
-        musicTitle.push(musicSrc[i].dataset.title);
-        imageSrc.push(musicSrc[i].dataset.img);
-        musicTrack.push(musicSrc[i].dataset.track);
-    }
-
-    // Add the music file.
-    m1 = new files(musicS[0], imageSrc[0], musicTitle[0], musicTrack[0]);
-    m2 = new files(musicS[1], imageSrc[1], musicTitle[1], musicTrack[1]);
-    m3 = new files(musicS[2], imageSrc[2], musicTitle[2], musicTrack[2]);
-
-    // Creat music playlist
-    let playList = [m1, m2, m3];
-    let audio = document.querySelector('audio');
-    let playit = 0;
-    let playing = playList[playit];
-    audio.setAttribute('src', playing.musicSource);
-    audio.play();
-    
-    function nextPrev() {
-    // Put the music information on the DOM
-        document.querySelector('.singer-image').src = playing.Image;
-        document.querySelector('.music-title').innerHTML = playing.track +'. '+ playing.title;
-    }
-
-    //Working on the controllers
-    (function(){
-        let playBtn = document.querySelector('.fa-play-circle');
-        let pauseBtn = document.querySelector('.fa-pause-circle');
-        let nextBtn = document.querySelector('.fa-forward');
-        let priviesBtn = document.querySelector('.fa-backward');
-        playBtn.addEventListener('click', ()=>{
-            audio.play();
-       });
-
-        pauseBtn.addEventListener('click', ()=>{
-            audio.pause();
-        });
-        
-        nextBtn.addEventListener('click', ()=>{
-            if(playit < playList.length){
-                playit++; 
-            }if (playit >= playList.length){
-                playit = 0;
-            }
-            playing = playList[playit];
-            audio.setAttribute('src',playing.musicSource);
-            audio.play();
-            nextPrev();
-
-        });
-
-        priviesBtn.addEventListener('click',()=> {
-            if (playit > 0) {
-                playit--;
-            }else if(playit <= 0){
-                playit = playList.length-1;
-            }
-            
-            playing = playList[playit];
-            audio.setAttribute('src',playing.musicSource);
-            audio.play();
-            nextPrev();
-        });
-        
-        nextPrev();
-    })();
-    
-
+//Track constructor function.
+function Track(track, title, src, Image, artist) {
+    this.src = src;
+    this.Image = Image;
+    this.title = title;
+    this.track = track;
+    this.artist = artist;
 }
+
+let musicSrc = document.querySelectorAll('audio source');
+let playList = [];
+musicSrc.forEach((src, i) => {
+    let truck = new Track(src.dataset.track, src.dataset.title, src.dataset.src, src.dataset.img, src.dataset.title.artist);
+    playList.push(truck);
+});
+
+// Creat music playlist
+let audio = document.querySelector('audio');
+let playing = playList[0];
+let playit = 0
+audio.setAttribute('src', playing.src);
+document.querySelector('.singer-image').src = playing.Image;
+document.querySelector('.music-title').innerText = playing.track + '. ' + playing.title;
+
+function nextPrev() {
+    playing = playList[playit];
+    audio.src = playing.src;
+    audio.play();
+    // Put the track information on the DOM
+    document.querySelector('.singer-image').src = playing.Image;
+    document.querySelector('.music-title').innerText = playing.track + '. ' + playing.title;
+}
+
+//Working on the controllers
+function Play() {
+    let playBtn = document.querySelector('.fa-play-circle');
+    let pauseBtn = document.querySelector('.fa-pause-circle');
+    let nextBtn = document.querySelector('.fa-forward');
+    let priviesBtn = document.querySelector('.fa-backward');
+    playBtn.addEventListener('click', () => {
+        audio.play();
+    });
+    pauseBtn.addEventListener('click', () => {
+        audio.pause();
+    });
+    nextBtn.addEventListener('click', () => {
+        if (playit < playList.length) {
+            playit++;
+        } if (playit >= playList.length) {
+            playit = 0;
+        }
+        nextPrev();
+    });
+    priviesBtn.addEventListener('click', () => {
+        if (playit > 0) {
+            playit--;
+        } else if (playit <= 0) {
+            playit = playList.length - 1;
+        }
+        nextPrev();
+    });
+}
+Play();
